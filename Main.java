5f677667747620dbae885a08e64ff6d31c361f14
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-02-14 16:56:26 +0000

    mas cosas

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     system.out.println("introducir temperatura: ");
+    
             temperatura = Integer.parseInt(in.readline());
             if(temperatura < 2){
               system.out.printLn("Riesgo de hielo");
