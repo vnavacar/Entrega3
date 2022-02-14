7044ad8c39f913b7363b7296a69aed90a0cc4013
vnavacar <98817213+vnavacar@users.noreply.github.com>
2022-02-14 17:25:11 +0000

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Ejercicio1 {
+public class Ejercicio3 {
   public static void main(String[] args) {
 
     //int temperatura;
@@ -11,40 +11,40 @@ public class Ejercicio1 {
             //temperatura = Integer.parseInt(in.readline());
     switch(temperatura){
             case 1:
-      System.out.println("Mes:   ");
+      System.out.println("Mes: Enero ");
       break;
             case 2:
-      System.out.println("Mes:   ");
+      System.out.println("Mes:Febrero   ");
       break;
             case 3:
-      System.out.println("Mes:   ");
+      System.out.println("Mes:Marzo   ");
       break;
             case 4:
-      System.out.println("Mes:   ");
+      System.out.println("Mes:Abril   ");
       break;
             case 5:
-      System.out.println("Mes:   ");
+      System.out.println("Mes:Mayo   ");
       break;
             case 6:
-      System.out.println("Mes:   ");
+      System.out.println("Mes: Junio  ");
       break;
             case 7:
-      System.out.println("Mes:   ");
+      System.out.println("Mes: Julio  ");
       break;
             case 8:
-      System.out.println("Mes:   ");
+      System.out.println("Mes:Agosto   ");
       break;
             case 9:
-      System.out.println("Mes:   ");
+      System.out.println("Mes: Septiembre  ");
       break;
             case 10:
-      System.out.println("Mes:   ");
+      System.out.println("Mes:Octubre   ");
       break;
             case 11:
-      System.out.println("Mes:   ");
+      System.out.println("Mes: Noviembre  ");
       break;
             case 12:
-      System.out.println("Mes:   ");
+      System.out.println("Mes: Diciembre  ");
       break;
       default:
       System.out.println("Eso no es un mes");
--- Ejercicio4.java
+++ Ejercicio4.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class Ejercicio4 {
   public static void main(String[] args) {
 String tipo;
-    
+    String modo;
+    int peso;
+    Scanner teclado=new Scanner(System.in);
+    System.out.println("¿Qué tipo de carne vas a cocinar?");
+tipo=teclado.nextLine();
+System.out.println("¿Cómo de cocinada la quieres?");
 
    
   }
